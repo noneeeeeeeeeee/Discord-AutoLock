ded7cd97810b202db3d7724588211b7c3dd7ad72
Vendouple <soupnoodle.buystuffhere@gmail.com>
2024-08-21 21:26:02 +0700

    Initial Commit

---
+++ Discord-AutoLock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Discord-AutoLock_FILE D:/dev/Github/Discord-AutoLock/Discord-AutoLock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Discord-AutoLock.exe)
+set(__QT_DEPLOY_TARGET_Discord-AutoLock_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Discord-AutoLock_RUNTIME_DLLS D:/dev/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/dev/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/dev/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
